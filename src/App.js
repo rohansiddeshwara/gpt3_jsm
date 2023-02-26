bd527a92205e1202a3e9236f5830c854b59b98c5
Rohan s <rohansiddeshwara@gmail.com>
2023-02-26 01:36:15 +0000

    adding project structures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
-
+import { Footer,Blog,possibility,Features,Header,WhatGPT3 } from './containers/indes';
+import { Cta,Brand,Navbar } from './components';
 
 function App() {
   return (
   <div>
-    <h1>GPT-3</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
   </div>
   );
 }
